6955cee6594d7c6ed34165c0002a94809626922c
SoominYim <soo15soo@kakao.com>
2024-05-14 17:42:55 +0900

    feat : 기초 사다리 update

--- components/ladder/canvas.vue
+++ components/ladder/canvas.vue
@@ -1,5 +1,5 @@
 <template>
-  <canvas id="canvas" :style="{ width: '600px', height: '330px' }"></canvas>
+  <canvas id="canvas" width="1920" height="1080" :style="{ width: '600px', height: '330px' }"></canvas>
 </template>
 
 <script lang="ts" setup>
@@ -14,28 +14,38 @@
   }
 
   function ladder_draw(ctx: any) {
-    for (let i = 1; i <= count.value; i++) {
-      ctx.lineWidth = 4;
-      ctx.strokeStyle = "#fff";
-
+    ctx.lineWidth = 30;
+    ctx.strokeStyle = "#fff";
+    for (let i = 0; i <= count.value - 1; i++) {
       ctx.beginPath();
-      ctx.moveTo(12.5 * (2 * i - 1) - 0.5, 0);
-      ctx.lineTo(12.5 * (2 * i - 1) - 0.5, 330);
+      ctx.moveTo(80 + i * 160, 0);
+      ctx.lineTo(80 + i * 160, 1076);
       ctx.stroke();
       ctx.beginPath();
     }
-    for (let j = 0; j <= 1; j++) {
-      Array.from({ length: count.value - 1 }, (_, i) => {
-        const r = getRandomNumber(14, 140);
-        ctx.lineWidth = 4;
-        ctx.strokeStyle = "#3f3";
-        ctx.beginPath();
-        ctx.moveTo(12.5 + 25 * i, r);
-        ctx.lineTo(37.5 + 25 * i, r);
-        ctx.stroke();
-        ctx.beginPath();
-      });
-    }
+
+    // for (let i = 1; i <= count.value; i++) {
+    //   ctx.lineWidth = 20;
+    //   ctx.strokeStyle = "#fff";
+
+    //   ctx.beginPath();
+    //   ctx.moveTo(12.5 * (2 * i - 1) - 0.5, 0);
+    //   ctx.lineTo(12.5 * (2 * i - 1) - 0.5, 330);
+    //   ctx.stroke();
+    //   ctx.beginPath();
+    // }
+    // for (let j = 0; j <= 1; j++) {
+    //   Array.from({ length: count.value - 1 }, (_, i) => {
+    //     const r = getRandomNumber(14, 140);
+    //     ctx.lineWidth = 4;
+    //     ctx.strokeStyle = "#3f3";
+    //     ctx.beginPath();
+    //     ctx.moveTo(12.5 + 25 * i, r);
+    //     ctx.lineTo(37.5 + 25 * i, r);
+    //     ctx.stroke();
+    //     ctx.beginPath();
+    //   });
+    // }
   }
 
   onMounted(() => {
